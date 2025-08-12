2c1cbbc413293c74d49273760d5020a36c95790d
AmandaZ2106 <romeogcruz@outlook.com>
2025-08-12 19:53:37 +0100

    Project

---
+++ Project.py
@@ -0,0 +1,7 @@
+congratulation="You're an amazing sportsperson!"
+print("Congratulations on the gold medal,Ram\n")
+print("Here's a trick:figure out what I wrote\n")
+revcongratulation=congratulation[::-1]
+print(revcongratulation)
+
+
