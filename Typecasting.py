03250fbd2ebeaa9a0e7bb3de65d0f52ae69f0f97
AmandaZ2106 <romeogcruz@outlook.com>
2025-08-12 19:08:28 +0100

    Typecasting

---
+++ Typecasting.py
@@ -0,0 +1,18 @@
+name="Ayden"
+age=4
+weight=16.2
+age_gap=True
+print("The Data types before typecasting")
+print("My brother's name is",name,type(name))
+print("He is ",age,"years old",type(age))
+print("He weighs",weight,"kg",type(weight))
+print("The age gap between us is 7. This is",age_gap,type(age_gap))
+print("The data types after typecasting")
+weight=int(weight)
+print("He weighs",weight,"kg",type(weight))
+
+
+
+
+
+
